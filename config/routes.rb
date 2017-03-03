3f7633ec5d2f5dbf8657881e0762ab732e7ac6b1
Samantha Morco <samanthamorco@gmail.com>
2017-03-02 19:13:00 -0500

    add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'meetings#index'
 
   get '/meetings' => 'meetings#index'
+  get '/meetings/new', to: 'meetings#new'
+  post '/meetings', to: 'meetings#create'
   get '/meetings/:id' => 'meetings#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
